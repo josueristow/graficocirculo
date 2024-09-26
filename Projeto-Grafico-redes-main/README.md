75da4b72f36a5d03f3c8558d670411cf553fbb8c
josueristow <126601767+josueristow@users.noreply.github.com>
2024-09-26 14:55:00 -0300

    Add files via upload

---
+++ Projeto-Grafico-redes-main/README.md
@@ -0,0 +1,1 @@
+# Projeto-Grafico-redes
---
+++ Projeto-Grafico-redes-main/graficoredes/grafico/graficos/common.js
@@ -0,0 +1,20 @@
+const getCSS = (variavel) => {
+    const bodyStyles = getComputedStyle(document.body)
+    return bodyStyles.getPropertyValue(variavel)
+}
+
+const tickConfig = {
+    family: getCSS('--font'),
+    size: 16,
+    color: getCSS('--primary-color')
+}
+
+function criarGrafico (data,layout) {
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, layout)
+
+}
+
+export { getCSS, tickConfig, criarGrafico }
